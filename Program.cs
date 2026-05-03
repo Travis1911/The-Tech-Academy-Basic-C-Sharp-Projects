052c4466d50dcdf1bb599f35bf2926c51e4b17e6
Jeethan1911 <Jeethan.Saldanha@my-aolcc.com>
2026-05-03 12:16:28 -0400

    Added Branching Assignment

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return;
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int dimensionTotal = width + height + length;
+
+            if (dimensionTotal > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                return;
+            }
+
+            int quote = (width * height * length * weight) / 100;
+
+            Console.WriteLine("Your estimated total for shipping this package is: $" + quote + ".00");
+            Console.WriteLine("Thank you!");
+        }
+    }
+}
