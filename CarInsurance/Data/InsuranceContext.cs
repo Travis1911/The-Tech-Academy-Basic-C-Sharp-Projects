04e753226d241c5c61144d9a1bc93ee008841bef
Travis1911 <Jeeth_1930@hotmail.com>
2026-06-08 16:51:26 -0400

    Add files via upload

---
+++ CarInsurance/Data/InsuranceContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarInsurance.Models;
+
+namespace CarInsurance.Data
+{
+    public class InsuranceContext : DbContext
+    {
+        public InsuranceContext(DbContextOptions<InsuranceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Insuree> Insurees { get; set; }
+    }
+}
