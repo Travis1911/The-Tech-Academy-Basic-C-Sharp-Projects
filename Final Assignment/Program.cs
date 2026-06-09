416ad1e6c3e67b655abd5a4eee9bc7223772e182
Travis1911 <Jeeth_1930@hotmail.com>
2026-06-09 06:35:08 -0400

    Add files via upload

---
+++ Final Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using StudentDatabase.Data;
+using StudentDatabase.Models;
+using System;
+
+namespace StudentDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new StudentContext())
+            {
+                var student = new Student
+                {
+                    StudentName = "John Smith"
+                };
+
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                Console.WriteLine("Student added successfully!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
