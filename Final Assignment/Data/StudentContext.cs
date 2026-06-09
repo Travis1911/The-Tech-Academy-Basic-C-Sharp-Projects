a3fbe146002d3f2897e0d62c61232941408ea8a8
Travis1911 <Jeeth_1930@hotmail.com>
2026-06-09 06:37:42 -0400

    Add files via upload

---
+++ Final Assignment/Data/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using StudentDatabase.Models;
+
+namespace StudentDatabase.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext()
+            : base("StudentDB")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
